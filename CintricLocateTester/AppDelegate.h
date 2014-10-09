a6db7e671eba4358ee216c925276abe2c9fc97f2
Joel Green <thejoelgreen@gmail.com>
2014-10-09 03:17:30 -0400

    Initial Commit

---
+++ CintricLocateTester/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  CintricLocateTester
+//
+//  Created by Joel Green on 10/9/14.
+//  Copyright (c) 2014 Joel Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ CintricLocateTester/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CintricLocateTester
+//
+//  Created by Joel Green on 10/9/14.
+//  Copyright (c) 2014 Joel Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
