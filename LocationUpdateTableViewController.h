02d4456ac305643a7e645042d237726792275e61
Joel Green <thejoelgreen@gmail.com>
2014-10-13 08:58:23 -0400

    added location update table view controller

---
+++ LocationUpdateTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  LocationUpdateTableViewController.h
+//  CintricLocateTester
+//
+//  Created by Joel Green on 10/13/14.
+//  Copyright (c) 2014 Joel Green. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CoreDataTableViewController.h"
+
+@interface LocationUpdateTableViewController : CoreDataTableViewController
+
+@end
